80e1e62ed6458fb73661a8a9ab8109d44a333d2a
pham0116 <pham0116@algonquinlive.com>
2025-11-09 21:33:35 -0500

    generate 0 or 1 randomly

--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@ function init(){
   const body = document.querySelector("body");
   
   body.addEventListener("click", function onClick() {
-    
+    let num = Math.floor(Math.random() * 2);
   });
 }
