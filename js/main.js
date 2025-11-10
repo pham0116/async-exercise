c24b824d920586149a30ab7f7f05078f0ab91838
pham0116 <pham0116@algonquinlive.com>
2025-11-09 21:32:13 -0500

    add click event listener on body

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 document.addEventListener('DOMContentLoaded', init);
 
+function init(){
+  const body = document.querySelector("body");
+  
+  body.addEventListener("click", function onClick() {
+    
+  });
+}
